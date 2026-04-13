392b00a5cc6074b210495b6c239ab9631fc0e823
Vivacity5X <chaitanykhandare789@gmail.com>
2026-04-13 11:54:56 +0530

    Initial commit - CNN Digit Recognition

---
+++ train.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+
+# Load dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Reshape for CNN
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+# Build CNN
+model = models.Sequential([
+    
+    # Convolution Layer 1
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    layers.MaxPooling2D((2,2)),
+
+    # Convolution Layer 2
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+
+    # Flatten
+    layers.Flatten(),
+
+    # Fully Connected
+    layers.Dense(128, activation='relu'),
+
+    # Dropout
+    layers.Dropout(0.5),
+
+    # Output
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+# Save model
+model.save("models/cnn_model.h5")
